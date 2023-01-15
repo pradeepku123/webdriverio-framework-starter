8c08a80ffcb9389294f7b9d3328ddbe5c89dc4de
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-01-15 11:16:43 +0530

    $

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,8 +1,12 @@
 describe("DES_01", () => {
   it("TC_001", async () => {
     console.log("Welcome To TC_001");
-    await browser.url(`http://localhost:3000`);
+    await browser.setTimeout({ implicit: 10000, pageLoad: 10000 });
     await browser.maximizeWindow();
-    await browser.debug(10000);
+    await browser.url(`http://localhost:3000`);
+
+    await $(`#username`).setValue(`Hello`);
+
+    await browser.pause(3000);
   });
 });
